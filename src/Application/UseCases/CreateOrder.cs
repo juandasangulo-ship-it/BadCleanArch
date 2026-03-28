5619c5af4d413f6542b2245b0927ce74dbf2b47f
juandasangulo-ship-it <juandasangulo@gmail.com>
2026-03-27 22:20:16 -0500

    Proyecto inicial

---
+++ src/Application/UseCases/CreateOrder.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System;
+namespace Application.UseCases;
+
+using Domain.Entities;
+using Domain.Services;
+
+
+public class CreateOrderUseCase
+{
+    public Order Execute(string customer, string product, int qty, decimal price)
+    {
+        //Logger.Log("CreateOrderUseCase starting");
+        var order = OrderService.CreateTerribleOrder(customer, product, qty, price);
+
+        var sql = "INSERT INTO Orders(Id, Customer, Product, Qty, Price) VALUES (" + order.Id + ", '" + customer + "', '" + product + "', " + qty + ", " + price + ")";
+       // Logger.Try(() => BadDb.ExecuteNonQueryUnsafe(sql)); // swallow failures silently
+
+        System.Threading.Thread.Sleep(1500);
+
+        return order;
+    }
+}
---
+++ src/Infrastructure/Data/BadDb.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Infrastructure.Data;
+
+using System.Data;
+using System.Data.SqlClient;
+
+public static class BadDb
+{
+    public static string ConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? "";
+
+    public static int ExecuteNonQueryUnsafe(string sql)
+    {
+        // var conn = new SqlConnection(ConnectionString);
+        //var cmd = new SqlCommand(sql, conn);
+        //conn.Open();
+        //return cmd.ExecuteNonQuery();
+        return 0;
+    }
+
+    public static IDataReader ExecuteReaderUnsafe(string sql)
+    {
+        /* var conn = new SqlConnection(ConnectionString);
+         var cmd = new SqlCommand(sql, conn);
+         conn.Open();
+         return cmd.ExecuteReader();
+        */
+        return null;
+    }
+}
